abaf4aa21a04d87304fbbb1754e352229835fb1d
Pranav Mali <pranavamali05@gmail.com>
2026-04-13 15:06:32 +0530

    commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root', 
-    password: 'Pranav@1210', 
+    password: '', 
     database: 'placement_db'
 });
 
